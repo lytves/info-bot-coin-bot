b460a027720b2b9855a9076a86558bb02848b81c
lytves <lytves@gmail.com>
2017-11-26 21:31:29 +0100

    test b2b

--- org/companyname/infobotcoinbot/infobotcoinbot.py
+++ org/companyname/infobotcoinbot/infobotcoinbot.py
@@ -33,7 +33,8 @@ def requestAPI():
     # bot.send_message(chatID, text)
     # time period each 3600 seconds = 1 hour
     # threading.Timer(3600, requestAPI).start()
-    bot.send_message("@CryptoCoinsInfoBot", "text")
+
+    bot.send_message("@CryptoCoinsInfoBot", text)
 
 requestAPI()
 

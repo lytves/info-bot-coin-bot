5a118b505be4609e50556aed8b5894d2fa5cb806
lytves <lytves@gmail.com>
2017-12-16 23:28:21 +0100

    new emojis for prices rates

--- org/companyname/infobotcoinbot/infobotcoinbot.py
+++ org/companyname/infobotcoinbot/infobotcoinbot.py
@@ -1,9 +1,9 @@
 import telebot
 import os
 import requests
-import time, threading
+import threading
 from flask import Flask, request
-import logging
+from emoji import emojize
 
 # set up config variables en your heroku environment
 # your bot TOKEN
@@ -18,21 +18,43 @@ bot = telebot.TeleBot(token)
 
 server = Flask(__name__)
 
-def requestAPI():
+def requestAPI(message):
     url = "https://api.coinmarketcap.com/v1/ticker/bitcoin"
     response = requests.get(url)
     name = response.json()[0]['name']
     price = response.json()[0]['price_usd']
-    rate24h = response.json()[0]['percent_change_24h']
-    rate7d = response.json()[0]['percent_change_7d']
-    text = "Current *" + name + "* price - *${}".format(price) + "*" \
-           + "\nLast 24 hours changed for: `" + rate24h + "%`" \
-           + "\nLast 7 days changed for: `" + rate7d + "%`"
-    bot.send_message(chatID, text, parse_mode="Markdown")
-    # time period each 3600 seconds = 1 hour
-    threading.Timer(3600, requestAPI).start()
 
-requestAPI()
+    # 24 hours price change with emoji
+    rate24h = response.json()[0]['percent_change_24h']
+    if float(rate24h) > 20:
+        rate24hemoji = emojize(":rocket:", use_aliases=True)
+    elif float(rate24h) < 0:
+        rate24hemoji = emojize(":small_red_triangle_down:", use_aliases=True)
+    elif float(rate24h) > 0:
+        rate24hemoji = emojize(":white_check_mark:", use_aliases=True)
+
+    # 7 days price change with emoji
+    rate7d = response.json()[0]['percent_change_7d']
+    if float(rate7d) > 20:
+        rate7demoji = emojize(":rocket:", use_aliases=True)
+    elif float(rate7d) < 0:
+        rate7demoji = emojize(":small_red_triangle_down:", use_aliases=True)
+    elif float(rate7d) > 0:
+        rate7demoji = emojize(":white_check_mark:", use_aliases=True)
+
+    text = "Current *" + name + "* price - *${}".format(price) + "*" \
+           + "\nLast 24 hours changed for: *" + rate24h + "%*" + rate24hemoji \
+           + "\nLast 7 days changed for: *" + rate7d + "%*" + rate7demoji
+
+    # bot.send_message(chatID, text, parse_mode="Markdown")
+    # time period each 3600 seconds = 1 hour
+    # threading.Timer(3600, requestAPI).start()
+
+    bot.send_message(message.from_user.id, text, parse_mode="Markdown")
+
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    requestAPI(message)
 
 @server.route("/bot", methods=['POST'])
 def getMessage():
